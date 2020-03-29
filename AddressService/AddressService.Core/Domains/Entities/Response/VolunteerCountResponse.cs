58253a85bd97d26730875f315994703f19ca6716
Jawwad Mukhtar <jawwad.mukhtar@gmail.com>
2020-03-29 14:15:42 +0100

    Add GetVolunteerCount

---
+++ AddressService/AddressService.Core/Domains/Entities/Response/VolunteerCountResponse.cs
@@ -0,0 +1,14 @@
+﻿using AddressService.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressService.Core.Domains.Entities.Response
+{
+    public class VolunteerCountResponse
+    {
+        public int ChampionCount { get; set; }
+        public int VolunteerCount { get; set; }
+
+    }
+}
---
+++ AddressService/AddressService.Handlers/GetVolunteerCountHandler.cs
@@ -0,0 +1,32 @@
+﻿using AddressService.Core.Domains.Entities.Request;
+using AddressService.Core.Domains.Entities.Response;
+using AddressService.Core.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AddressService.Handlers
+{
+    public class GetVolunteerCountHandler : IRequestHandler<GetVolunteerCountRequest, VolunteerCountResponse>
+    {
+        private readonly IRepository _repository;
+
+        public GetVolunteerCountHandler(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<VolunteerCountResponse> Handle(GetVolunteerCountRequest request, CancellationToken cancellationToken)
+        {
+            var response = new VolunteerCountResponse()
+            {
+                VolunteerCount = 1,
+                ChampionCount = 2
+            };
+            return Task.FromResult(response);
+        }
+    }
+}

a0cab377d609ee86a6ec3f492d5319fa066ebba1
Jawwad Mukhtar <jawwad.mukhtar@gmail.com>
2020-03-31 14:53:46 +0100

    Add Test function

---
+++ AddressService/AddressService.AzureFunction/Test.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using MediatR;
+using System;
+using AddressService.Core.Domains.Entities.Request;
+using AddressService.Core.Domains.Entities.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace AddressService.AzureFunction
+{
+    public class Test
+    {
+        [FunctionName("Test")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                log.LogInformation("C# HTTP trigger function processed a request.");
+
+                var config = new ConfigurationBuilder()
+        .SetBasePath(Environment.CurrentDirectory)
+        .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables()
+        .Build();
+
+                string connectionstring = config.GetConnectionString("SqlConnectionString");
+
+                return new OkObjectResult(connectionstring);
+            }
+            catch (Exception exc)
+            {
+                return new BadRequestObjectResult(exc);
+            }
+        }
+    }
+}

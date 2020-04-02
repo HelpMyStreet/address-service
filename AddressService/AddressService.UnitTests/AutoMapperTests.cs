7014247b8d9e67ac852df0f4dd3e733bf53f62d3
William Thomas <14827486+william-thomas@users.noreply.github.com>
2020-04-02 21:02:43 +0100

    Add AutoMapper config tests

---
+++ AddressService/AddressService.UnitTests/AutoMapperTests.cs
@@ -0,0 +1,23 @@
+﻿using AddressService.Mappers;
+using AutoMapper;
+using NUnit.Framework;
+
+namespace AddressService.UnitTests
+{
+    public  class AutoMapperTests
+    {
+
+        [Test]
+        public void AssertConfigurationIsValid()
+        {
+            MapperConfiguration configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<AddressDetailsProfile>();
+                cfg.AddProfile<PostCodeProfile>();
+            });
+
+            configuration.AssertConfigurationIsValid();
+
+        }
+    }
+}

45972022a00c26eab72ed264d42a768e9ca376aa
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-01-20 12:19:00 +0000

    Add lincoln locations

--- AddressService/AddressService.Repo/Migrations/20210120121727_AddLincolnLocations.Designer.cs
+++ AddressService/AddressService.Repo/Migrations/20210120121727_AddLincolnLocations.Designer.cs
@@ -4,14 +4,16 @@ using AddressService.Repo;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AddressService.Repo.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210120121727_AddLincolnLocations")]
+    partial class AddLincolnLocations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

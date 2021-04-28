71d5d0ce770333a14c6c7a2b46597fdeacf08f40
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-04-28 10:34:11 +0100

    change short name for stamford

---
+++ AddressService/AddressService.Repo/Migrations/20210428093308_ChangeShortNameForStamford.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AddressService.Repo.Migrations
+{
+    public partial class ChangeShortNameForStamford : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Address",
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: -6,
+                column: "ShortName",
+                value: "Stamford (St. Mary’s Medical Centre)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Address",
+                table: "Location",
+                keyColumn: "Id",
+                keyValue: -6,
+                column: "ShortName",
+                value: "Stamford");
+        }
+    }
+}
